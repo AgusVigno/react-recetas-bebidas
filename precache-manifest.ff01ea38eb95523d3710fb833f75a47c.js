e29b77420dfe47cbf027860d5dce66ba9e74ce46
Agustin Vignolo <agus88lp@gmail.com>
2020-06-01 01:07:02 -0300

    Updates

---
+++ precache-manifest.ff01ea38eb95523d3710fb833f75a47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacfcc4aee84e9a2a0def7320610e4ca",
+    "url": "/react-recetas-bebidas/index.html"
+  },
+  {
+    "revision": "404085082ba8aac70582",
+    "url": "/react-recetas-bebidas/static/css/main.3ea7020a.chunk.css"
+  },
+  {
+    "revision": "10cbaf7af8f67762a5e7",
+    "url": "/react-recetas-bebidas/static/js/2.cfb6ea78.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/react-recetas-bebidas/static/js/2.cfb6ea78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404085082ba8aac70582",
+    "url": "/react-recetas-bebidas/static/js/main.a8c26279.chunk.js"
+  },
+  {
+    "revision": "896fc662c2344212fbc8",
+    "url": "/react-recetas-bebidas/static/js/runtime-main.5f10b938.js"
+  }
+]);
